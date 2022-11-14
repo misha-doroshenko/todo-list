699de2d5e2ad33a7b7dd29ac0d7160619ab183f4
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-11-14 14:17:20 +0200

    Add tag list view

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from django.views.i18n import JavaScriptCatalog
 
-from app.views import TaskListView, TaskUpdateView, TaskCreateView
+from app.views import TaskListView, TaskUpdateView, TaskCreateView, TaskDeleteView, complete_undo, TagListView
 
 urlpatterns = [
      path("", TaskListView.as_view(), name="task-list"),
      path("jsi18n", JavaScriptCatalog.as_view(), name="js-catlog"),
      path("<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
      path("create/", TaskCreateView.as_view(), name="task-create"),
+     path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+     path("<int:pk>/complete-undo", complete_undo, name="task-complete-undo"),
+     path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "app"
--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
+from django.db.models import Q
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
 from app.forms import TaskForm
-from app.models import Task
+from app.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
@@ -21,3 +23,17 @@ class TaskCreateView(generic.CreateView):
     model = Task
     form_class = TaskForm
     success_url = reverse_lazy("app:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("app:task-list")
+
+
+def complete_undo(request, pk):
+    Task.objects.filter(id=pk).update(done=Q(done=False))
+    return HttpResponseRedirect(reverse_lazy("app:task-list"))
+
+
+class TagListView(generic.ListView):
+    model = Tag
