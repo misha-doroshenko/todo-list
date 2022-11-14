8f7cf8afadee3dd779fb2731b786bcca568c8d73
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-11-14 10:47:36 +0200

    Rename models

--- app/models.py
+++ app/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 
-class TagModel(models.Model):
+class Tag(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
 
-class TaskModel(models.Model):
+class Task(models.Model):
     content = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(null=True, blank=True)
     done = models.BooleanField(default=False)
-    tags = models.ManyToManyField(TagModel, related_name="tasks")
+    tags = models.ManyToManyField(Tag, related_name="tasks")
